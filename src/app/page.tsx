6c63fe17c5397e8dd5d6c957587b741b2a4cecc8
IntoxicatedChildStudio <25576983+HuangKuoTsun@users.noreply.github.com>
2025-02-08 18:48:23 +0800

    try error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import JsonData from "@/components/LoadFromJson";
 
 export default async function Home() {
 
-  const PageData = await JsonData('/JSON/AppDescription.json');
+  const PageData = await JsonData("/JSON/AppDescription.json");
   
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">

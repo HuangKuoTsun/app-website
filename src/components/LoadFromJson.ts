59db0ee156761972ff7666acff9f488757d562a2
IntoxicatedChildStudio <25576983+HuangKuoTsun@users.noreply.github.com>
2025-02-10 10:40:34 +0800

    change path

--- src/components/LoadFromJson.ts
+++ src/components/LoadFromJson.ts
@@ -9,8 +9,8 @@ export interface JsonObject {
     Description: string;
 }
 
-// FilePath: string = '/JSON/AppDescription.json'
-export default async function JsonData( FilePath: string = '/JSON/AppDescription.json' ): Promise<JsonObject[]> {
+// FilePath: string = '/Application/Description.json'
+export default async function JsonData( FilePath: string = '/Application/Description.json' ): Promise<JsonObject[]> {
     //const file = await fs.readFile(process.cwd() + FilePath, 'utf8');
     const file = await fs.readFile(FilePath, 'utf8');
     const data: JsonObject[] = JSON.parse(file);

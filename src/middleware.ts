b8296bcce593412a790df1492ea66cb21b8a4026
IntoxicatedChildStudio <25576983+HuangKuoTsun@users.noreply.github.com>
2025-02-13 19:57:11 +0800

    Fix middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -25,22 +25,12 @@ export function middleware(request: NextRequest) {
         .trim()
     const requestHeaders = new Headers(request.headers);
     requestHeaders.set('x-nonce', nonce);
-    requestHeaders.set(
-        'Content-Security-Policy',
-        contentSecurityPolicyHeaderValue
-    );
+    requestHeaders.set('Content-Security-Policy',contentSecurityPolicyHeaderValue);
     const response = NextResponse.next({
         request: {
             headers: requestHeaders,
         },
     });
-    /*
-    response.headers.set(
-        'Content-Security-Policy',
-        contentSecurityPolicyHeaderValue
-    ); 
-    return NextResponse.next();
-    */
     return response;
 };
 
